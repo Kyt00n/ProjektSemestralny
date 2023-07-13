7e626f1d0ab19da08e5e6abb00be28e4744a0577
Eryk Olsza <eryk.olsza@microsoft.wsei.edu.pl>
2023-07-13 22:43:40 +0200

    main view skeleton

--- UserInterface/App.xaml.cs
+++ UserInterface/App.xaml.cs
@@ -13,5 +13,11 @@ namespace UserInterface
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ UserInterface/Views/MainControlView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace UserInterface.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainControlView.xaml
+    /// </summary>
+    public partial class MainControlView : UserControl
+    {
+        public MainControlView()
+        {
+            InitializeComponent();
+        }
+    }
+}
